8c5a38f776d274c29ecc5c980b8ce661adcd6da0
Holger Stenger <holger.stenger@gmx.de>
2021-02-05 12:07:31 +0100

    Extend unit test for PSLexer
    * Test handling of string literals

--- src/test/java/org/purescript/lexer/PSLexerTest.kt
+++ src/test/java/org/purescript/lexer/PSLexerTest.kt
@@ -1,6 +1,10 @@
 package org.purescript.lexer
 
+import com.intellij.lexer.Lexer
+import com.intellij.psi.tree.IElementType
 import junit.framework.TestCase
+import org.purescript.psi.PSElementType
+import org.purescript.psi.PSTokens
 
 class PSLexerTest : TestCase() {
     fun testItHandlesFileEndingInEmptyLine() {
@@ -12,5 +16,100 @@ class PSLexerTest : TestCase() {
         while (lexer.tokenEnd < lexer.bufferEnd) {
             lexer.advance()
         }
+    }
+
+    fun `test string literal with single character`() {
+        assertPSLexerProducesTokens(
+            """
+            "a"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\"")
+        )
+    }
+
+    fun `test string literal with two characters`() {
+        assertPSLexerProducesTokens(
+            """
+            "ab"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"ab\"")
+        )
+    }
+
+    fun `test string literal with escape sequence`() {
+        assertPSLexerProducesTokens(
+            """
+            "a\nb"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\\n\b\"")
+        )
+    }
+
+    fun `test string literal with ampersand gap`() {
+        assertPSLexerProducesTokens(
+            """
+            "a\&b"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\\&b\"")
+        )
+    }
+
+    fun `test string literal with whitespace gap`() {
+        assertPSLexerProducesTokens(
+            """
+            "a\
+            \b"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\\\n\\b\"")
+        )
+    }
+
+    fun `test two string literals right next to each other`() {
+        assertPSLexerProducesTokens(
+            """
+            "a""b"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\"", PSTokens.STRING to "\"b\"")
+        )
+    }
+
+    fun `test two string literals separated by whitespace`() {
+        assertPSLexerProducesTokens(
+            """
+            "a" "b"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\"", PSTokens.WS to " ", PSTokens.STRING to "\"b\"")
+        )
+    }
+
+    fun `test two string literals separated by a multi-line comment`() {
+        assertPSLexerProducesTokens(
+            """
+            "a"{- comment -}"b"
+            """.trimIndent(),
+            listOf(PSTokens.STRING to "\"a\"", PSTokens.STRING to "\"b\"")
+        )
+    }
+
+    private fun assertPSLexerProducesTokens(
+        input: String,
+        expectedTokens: List<Pair<PSElementType, String>>
+    ) {
+        val lexer = PSLexer()
+        lexer.start(
+            input
+        )
+        val tokens = toTokenList(lexer)
+        assertEquals(expectedTokens, tokens)
+    }
+
+    private fun toTokenList(lexer: Lexer): List<Pair<IElementType, String>> {
+        val tokens = mutableListOf<Pair<IElementType, String>>()
+        while (true) {
+            val tokenType = lexer.tokenType ?: break
+            tokens.add(tokenType to lexer.tokenText)
+            lexer.advance()
+        }
+        return tokens;
     }
 }
